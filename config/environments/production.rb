04f8a5c03d6ccbe42d6c2a65dbb62afcdd48b71f
Michael Christenson II <michael@rebelhold.com>
2011-12-22 17:35:28 -0700

    setup production mailer options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,16 +47,17 @@ RebelFoundation::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     :address        => "smtp.sendgrid.net",
     :port           => "25",
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => ENV['SENDGRID_DOMAIN']
-  }  
-  config.action_mailer.default_url_options = { :host => 'example.com' }
-  
+  }
+
+  config.action_mailer.default_url_options = { :host => 'communal.herokuapp.com' }
+  config.action_mailer.delivery_method     = :smtp
 
   # Enable threaded mode
   # config.threadsafe!
