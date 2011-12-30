9bf38af893ffc585eb6a6df836a6daa109a6414a
Michael Christenson II <michael@rebelhold.com>
2011-12-29 17:49:00 -0700

    added logout

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,4 +6,9 @@ class SessionController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 RebelFoundation::Application.routes.draw do
   resources :users
   resource  :session
+  match '/logout' => 'session#destroy', as: :logout
 
   # OAuth how you humor me so ...
   match '/auth/:provider/callback' => 'session#create'
