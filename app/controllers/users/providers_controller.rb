44ccb908eba77b5e08c13a8d019ee106c5ddcb8a
Michael Christenson II <michael@rebelhold.com>
2012-01-02 11:26:36 -0700

    fixed path typo\ check for provider_id

--- app/controllers/users/providers_controller.rb
+++ app/controllers/users/providers_controller.rb
@@ -7,11 +7,11 @@ class Users::ProvidersController < ApplicationController
 
   def update
     session[:redirect_to] = profile_path
-    redirect_to "/auth/#{params[:id]}"
+    redirect_to "/auth/#{params[:provider_id] ? params[:provider_id] : params[:id]}"
   end
 
   def destroy
-    @current_user.providers.where(name: params[:id]).destroy_all
+    @current_user.providers.where(name: (params[:provider_id] ? params[:provider_id] : params[:id])).destroy_all
     redirect_to profile_path
   end
 end
